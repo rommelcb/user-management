06732cfe2b98e6fd8c8a28af7a76e286259900ca
Rommel Blas <rommel.blas@gmail.com>
2022-07-05 02:05:54 +0800

    Add basic server functionality.

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import http from 'http';
 import express from 'express';
 import mongoose from 'mongoose';
 import { config } from './config/config';
@@ -30,5 +31,31 @@ const StartServer = () => {
   router.use(express.urlencoded({ extended: true }));
   router.use(express.json());
 
-  
+  /** API Rules */
+  router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+    }
+
+    next();
+  });
+
+  /** Routes */
+
+  /** Healthcheck */
+  router.get('/health', (req, res, next) => res.status(200).json({ message: 'OK' }));
+
+  /** Error Handling */
+  router.use((req, res, next) => {
+    const error = new Error('Not Found');
+    console.log(error);
+
+    return res.status(404).json({ message: error.message });
+  });
+
+  http.createServer(router).listen(config.server.port, () => console.log(`Server is running on port ${config.server.port}.`));
 };
