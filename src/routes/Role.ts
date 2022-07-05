5630856935517ec5371d09cf4ad3ee7c6355e356
Rommel Blas <rommel.blas@gmail.com>
2022-07-05 18:57:18 +0800

    Added the validation middleware to the routes.

--- src/routes/Role.ts
+++ src/routes/Role.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import controller from '../controllers/Role';
+import { Schemas, ValidateSchema } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
-router.post('/', controller.createRole);
+router.post('/', ValidateSchema(Schemas.role.create), controller.createRole);
 router.get('/:id', controller.readRole);
 router.get('/', controller.readAllRole);
-router.patch('/:id', controller.updateRole);
+router.patch('/:id', ValidateSchema(Schemas.role.update), controller.updateRole);
 router.delete('/:id', controller.deleteRole);
 
 export = router;
--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import controller from '../controllers/User';
+import { Schemas, ValidateSchema } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
-router.post('/', controller.createUser);
+router.post('/', ValidateSchema(Schemas.user.create), controller.createUser);
 router.get('/:id', controller.readUser);
 router.get('/', controller.readAllUser);
-router.patch('/:id', controller.updateUser);
+router.patch('/:id', ValidateSchema(Schemas.role.update), controller.updateUser);
 router.delete('/:id', controller.deleteUser);
 
 export = router;
